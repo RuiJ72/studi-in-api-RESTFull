b3037c176ac2a18e31f403cb45c5602b0d46fb50
RuiJ72 <rlagos900@gmail.com>
2023-10-17 14:52:11 -0300

    Method GetById implementation

--- src/main/java/com/lagoscoutinho/api/controller/ClienteController.java
+++ src/main/java/com/lagoscoutinho/api/controller/ClienteController.java
@@ -1,30 +1,34 @@
 package com.lagoscoutinho.api.controller;
 
 import com.lagoscoutinho.api.domain.model.Cliente;
+import com.lagoscoutinho.api.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
     @GetMapping("/clientes")
     public List<Cliente> listar() {
 
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Rui Lagos");
-        cliente1.setTelefone("11-95845-1111");
-        cliente1.setEmail("lagos@gmail.com");
+        return clienteRepository.findAll();
 
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Jorge Coutinho");
-        cliente2.setTelefone("11-9888-1111");
-        cliente2.setEmail("coutinho@gmail.com");
+    }
+    @GetMapping("/clientes/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+        return clienteRepository.findById(clienteId)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                .orElse(ResponseEntity.notFound().build());
 
-        return Arrays.asList(cliente1, cliente2);
     }
 }
