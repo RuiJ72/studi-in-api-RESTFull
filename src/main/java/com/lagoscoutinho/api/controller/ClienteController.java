8c433af023166c3764e904b963bbbba14891a347
RuiJ72 <rlagos900@gmail.com>
2023-10-19 12:39:54 -0300

    Method DELETE implemented

--- src/main/java/com/lagoscoutinho/api/controller/ClienteController.java
+++ src/main/java/com/lagoscoutinho/api/controller/ClienteController.java
@@ -35,4 +35,28 @@ public class ClienteController {
     public Cliente adicionar(@RequestBody Cliente cliente) {
         return clienteRepository.save(cliente);
     }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+                                             @RequestBody Cliente cliente) {
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(clienteId);
+        cliente = clienteRepository.save(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{clienteId}")
+    public  ResponseEntity<Void> remover(@PathVariable long clienteId) {
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        clienteRepository.deleteById(clienteId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
