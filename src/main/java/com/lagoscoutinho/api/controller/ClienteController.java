df6f84302feb692f19352dbcee78c56dd5efba58
RuiJ72 <rlagos900@gmail.com>
2023-10-19 09:00:38 -0300

    POST method created

--- src/main/java/com/lagoscoutinho/api/controller/ClienteController.java
+++ src/main/java/com/lagoscoutinho/api/controller/ClienteController.java
@@ -4,31 +4,35 @@ import com.lagoscoutinho.api.domain.model.Cliente;
 import com.lagoscoutinho.api.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar() {
 
         return clienteRepository.findAll();
 
     }
-    @GetMapping("/clientes/{clienteId}")
+    @GetMapping("/{clienteId}")
     public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
         return clienteRepository.findById(clienteId)
                 .map(cliente -> ResponseEntity.ok(cliente))
                 .orElse(ResponseEntity.notFound().build());
-
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
     }
 }
